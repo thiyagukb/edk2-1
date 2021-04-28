eb65d541a766b64651141e6d2dd4828bef8527d9
Ray Ni <ray.ni@intel.com>
2021-04-28 15:36:34 +0800

    Add command (in comment) to demo how to add FV as section to ELF Signed-off-by: Ray Ni <ray.ni@intel.com>

--- build_qemu_pol.bat
+++ build_qemu_pol.bat
@@ -5,3 +5,7 @@ call py -3 UefiPayloadPkg\tools\pack_payload.py -i Build\UefiPayloadPkgX64\DEBUG
 if not %ERRORLEVEL% == 0 exit /b 1
 
 call build -p OvmfPkg\OvmfPkgPol.dsc -a IA32 -a X64 -D DEBUG_ON_SERIAL_PORT -t VS2019 -y ovmflog.txt
+
+
+@REM llvm-objcopy -I elf32-i386 -O elf32-i386 --add-section .upld.uefi_fv=Build\UefiPayloadPkgX64\DEBUG_VS2019\FV\DXEFV.Fv Build\UefiPayloadPkgX64\DEBUG_CLANGELFPDB\IA32\UniversalPayloadEntry.efi
+@REM llvm-objcopy -I elf32-i386 -O elf32-i386 --set-section-alignment .upld.uefi_fv=4096 Build\UefiPayloadPkgX64\DEBUG_CLANGELFPDB\IA32\UniversalPayloadEntry.efi
