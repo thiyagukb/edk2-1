LiuZhiguang001/edk2

UefiPayloadPkg: Remove code that installs APCI
MdeModulePkg\Universal\Acpi\AcpiTableDxe\AcpiTableDxe.inf is capable to install
ACPI table contained in HOB. All ACPI table shoulb be managed by that module.

Cc: Maurice Ma <maurice.ma@intel.com>
Cc: Guo Dong <guo.dong@intel.com>
Cc: Benjamin You <benjamin.you@intel.com>
Cc: Ray Ni <ray.ni@intel.com>
Signed-off-by: Zhiguang Liu <zhiguang.liu@intel.com>

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -235,6 +235,7 @@ BuildHobFromBl (
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB GfxDeviceInfo;
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB *NewGfxDeviceInfo;
   PLD_SMBIOS_TABLE_HOB             *SmBiosTableAdress;
+  PLD_ACPI_TABLE_HOB               *AcpiTableAdress;
 
   //
   // Parse memory info and build memory HOBs
@@ -284,6 +285,14 @@ BuildHobFromBl (
   ASSERT (SmBiosTableAdress != NULL);
   SmBiosTableAdress->SmBiosTableAddress = SysTableInfo.SmbiosTableBase;
   DEBUG ((DEBUG_INFO, "Create smbios table gPldSmbiosTableGuid guid hob\n"));
+
+  // 
+  // Creat ACPI table Hob
+  //
+  AcpiTableAdress = BuildGuidHob (&gPldAcpiTableGuid, sizeof (PLD_ACPI_TABLE_HOB));
+  ASSERT (AcpiTableAdress != NULL);
+  AcpiTableAdress->Rsdp = SysTableInfo.AcpiTableBase;
+  DEBUG ((DEBUG_INFO, "Create smbios table gPldAcpiTableGuid guid hob\n"));
 
   //
   // Create guid hob for acpi board information

thiyagukb/edk2-1

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -235,6 +235,7 @@ BuildHobFromBl (
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB GfxDeviceInfo;
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB *NewGfxDeviceInfo;
   PLD_SMBIOS_TABLE_HOB             *SmBiosTableHob;
+  PLD_ACPI_TABLE_HOB               *AcpiTableHob;
 
   //
   // Parse memory info and build memory HOBs
@@ -284,6 +285,14 @@ BuildHobFromBl (
   ASSERT (SmBiosTableHob != NULL);
   SmBiosTableHob->SmBiosEntryPoint = SysTableInfo.SmbiosTableBase;
   DEBUG ((DEBUG_INFO, "Create smbios table gPldSmbiosTableGuid guid hob\n"));
+
+  // 
+  // Creat ACPI table Hob
+  //
+  AcpiTableHob = BuildGuidHob (&gPldAcpiTableGuid, sizeof (PLD_ACPI_TABLE_HOB));
+  ASSERT (AcpiTableHob != NULL);
+  AcpiTableHob->Rsdp = SysTableInfo.AcpiTableBase;
+  DEBUG ((DEBUG_INFO, "Create smbios table gPldAcpiTableGuid guid hob\n"));
 
   //
   // Create guid hob for acpi board information