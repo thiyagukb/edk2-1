fe94a0f78653007c4bbf3257aa6a2197c7ae932d
Ray Ni <ray.ni@intel.com>
2021-05-01 21:54:52 +0800

    Update build script for 64bit ELF payload in OVMF Signed-off-by: Ray Ni <ray.ni@intel.com>

--- build_qemu_pol.bat
+++ build_qemu_pol.bat
@@ -1,11 +1,39 @@
-call build -p UefiPayloadPkg\UefiPayloadPkg.dsc -a IA32 -a X64 -D UNIVERSAL_PAYLOAD=TRUE -t VS2019 -y upllog.txt
+@setlocal
+
+@if "%1" == "entry" (
+  goto entry
+)
+@if "%1" == "objcopy" (
+  goto objcopy
+)
+
+@if "%1" == "ovmf" (
+  goto ovmf
+)
+
+call build -p UefiPayloadPkg\UefiPayloadPkg.dsc -a X64 -D UNIVERSAL_PAYLOAD=TRUE -t VS2019 -y upllog.txt
 if not %ERRORLEVEL% == 0 exit /b 1
 
-call py -3 UefiPayloadPkg\tools\pack_payload.py -i Build\UefiPayloadPkgX64\DEBUG_VS2019\FV\UEFIPAYLOAD.fd -a 4096 -ai -o UPL.bin
+:entry
+call build -p UefiPayloadPkg\UefiPayloadPkg.dsc -a X64 -D UNIVERSAL_PAYLOAD=TRUE -m UefiPayloadPkg\UefiPayloadEntry\UniversalPayloadEntry.inf -t CLANGDWARF 
 if not %ERRORLEVEL% == 0 exit /b 1
 
+
+:objcopy
+@set FV=Build\UefiPayloadPkgX64\DEBUG_VS2019\FV\DXEFV.Fv
+@set ENTRY=Build\UefiPayloadPkgX64\DEBUG_CLANGDWARF\X64\UefiPayloadPkg\UefiPayloadEntry\UniversalPayloadEntry\DEBUG\UniversalPayloadEntry.elf
+
+@REM 
+@REM If dependent sources are not changed, entry.elf isn't updated by build tool.
+@REM So, if we don't remove the uefi_fv section before adding, there will be 2 uefi_fv sections.
+@REM
+Remove .upld.uefi_fv section because entry.elf isn't updated if dependent sources are not changed.
+llvm-objcopy -I elf64-x86-64 -O elf64-x86-64 --remove-section .upld.uefi_fv %ENTRY%
+@if not %ERRORLEVEL% == 0 exit /b 1
+llvm-objcopy -I elf64-x86-64 -O elf64-x86-64 --add-section .upld.uefi_fv=%FV% %ENTRY%
+@if not %ERRORLEVEL% == 0 exit /b 1
+llvm-objcopy -I elf64-x86-64 -O elf64-x86-64 --set-section-alignment .upld.uefi_fv=8 %ENTRY%
+@if not %ERRORLEVEL% == 0 exit /b 1
+
+:ovmf
 call build -p OvmfPkg\OvmfPkgPol.dsc -a IA32 -a X64 -D DEBUG_ON_SERIAL_PORT -t VS2019 -y ovmflog.txt
-
-
-@REM llvm-objcopy -I elf32-i386 -O elf32-i386 --add-section .upld.uefi_fv=Build\UefiPayloadPkgX64\DEBUG_VS2019\FV\DXEFV.Fv Build\UefiPayloadPkgX64\DEBUG_CLANGELFPDB\IA32\UniversalPayloadEntry.efi
-@REM llvm-objcopy -I elf32-i386 -O elf32-i386 --set-section-alignment .upld.uefi_fv=4096 Build\UefiPayloadPkgX64\DEBUG_CLANGELFPDB\IA32\UniversalPayloadEntry.efi
