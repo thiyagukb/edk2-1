3a4dbd5902b2e47aa6f2802c56ec0264b397dc26
Ray Ni <ray.ni@intel.com>
2021-05-01 20:17:45 +0800

    MdePkg/UniversalPayload: Add definition for extra info in payload
    The payload is in ELF format per the universal payload spec.
    PLD_INFO_HEADER is stored in the ELF payload as a separate section
    named ".upld_info".
    
    Extra data needed by payload is stored in sections whose name starts
    with ".upld.".
    
    Signed-off-by: Ray Ni <ray.ni@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <gaoliming@byosoft.com.cn>
    Cc: Zhiguang Liu <zhiguang.liu@intel.com>
    Cc: Maurice Ma <maurice.ma@intel.com>
    Cc: Guo Dong <guo.dong@intel.com>

--- MdePkg/Include/UniversalPayload/UniversalPayload.h
+++ MdePkg/Include/UniversalPayload/UniversalPayload.h
@@ -9,11 +9,30 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #ifndef __UNIVERSAL_PAYLOAD_H__
 #define __UNIVERSAL_PAYLOAD_H__
 
+typedef  VOID   (EFIAPI *UNIVERSAL_PAYLOAD_ENTRY) (VOID *HobList);
+
+#define PLD_IDENTIFIER                   SIGNATURE_32('U', 'P', 'L', 'D')
+#define PLD_INFO_SEC_NAME                ".upld_info"
+#define PLD_EXTRA_SEC_NAME_PREFIX        ".upld."
+#define PLD_EXTRA_SEC_NAME_PREFIX_LENGTH (sizeof (PLD_EXTRA_SEC_NAME_PREFIX) - 1)
+
 #pragma pack(1)
 
 typedef struct {
-  UINT8                Revision;
-  UINT8                Reserved[3];
+  UINT32                          Identifier;
+  UINT32                          HeaderLength;
+  UINT16                          SpecRevision;
+  UINT8                           Reserved[2];
+  UINT32                          Revision;
+  UINT32                          Attribute;
+  UINT32                          Capability;
+  CHAR8                           ProducerId[16];
+  CHAR8                           ImageId[16];
+} PLD_INFO_HEADER;
+
+typedef struct {
+  UINT8 Revision;
+  UINT8 Reserved[3];
 } PLD_GENERIC_HEADER;
 
 #pragma pack()
