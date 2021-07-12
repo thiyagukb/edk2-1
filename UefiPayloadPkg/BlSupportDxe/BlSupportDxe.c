UniversalScalableFirmware/edk2

UefiPayloadPkg: Use ACPI protocol and SMBIOS DXE
ACPI DXE driver will consume the ACPI HOB to install ACPI protocol.
and SMBIOS DXE driver will consume SMBIOS HOB.
So update UEFI payload use use these common drivers.

Signed-off-by: Guo Dong <guo.dong@intel.com>

UefiPayloadPkg/BlSupportDxe/BlSupportDxe.c
@@ -176,34 +176,19 @@ BlDxeEntryPoint (
   EFI_STATUS Status;
   EFI_HOB_GUID_TYPE          *GuidHob;
   EFI_PEI_GRAPHICS_INFO_HOB  *GfxInfo;
-  ACPI_TABLE_HOB             *AcpiTableHob;
-  SMBIOS_TABLE_HOB           *SmbiosTable;
+  PLD_ACPI_TABLE_HOB         *AcpiTableHob;
 
   Status = EFI_SUCCESS;
 
   //
   // Install Acpi Table
   //
-  GuidHob = GetFirstGuidHob (&gEfiAcpiTableGuid);
+  GuidHob = GetFirstGuidHob (&gPldAcpiTableGuid);
   ASSERT (GuidHob != NULL);
   if (GuidHob != NULL) {
-    AcpiTableHob = (ACPI_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
-    DEBUG ((DEBUG_ERROR, "Install Acpi Table at 0x%lx \n", AcpiTableHob->TableAddress));
-    Status = gBS->InstallConfigurationTable (&gEfiAcpiTableGuid, (VOID *)(UINTN)AcpiTableHob->TableAddress);
-    ASSERT_EFI_ERROR (Status);
-
-    Status = SetPcdsUsingAcpiTable (AcpiTableHob->TableAddress);
-    ASSERT_EFI_ERROR (Status);
-  }
-
-  //
-  // Install Smbios Table
-  //
-  GuidHob = GetFirstGuidHob (&gEfiSmbiosTableGuid);
-  if (GuidHob != NULL) {
-    SmbiosTable = (SMBIOS_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
-    DEBUG ((DEBUG_ERROR, "Install SMBIOS Table at 0x%lx \n", SmbiosTable->TableAddress));
-    Status = gBS->InstallConfigurationTable (&gEfiSmbiosTableGuid, (VOID *)(UINTN)SmbiosTable->TableAddress);
+    AcpiTableHob = (PLD_ACPI_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
+    DEBUG ((DEBUG_ERROR, "Install Acpi Table at 0x%lx \n", AcpiTableHob->Rsdp));
+    Status = SetPcdsUsingAcpiTable ((UINT64)(UINTN)AcpiTableHob->Rsdp);
     ASSERT_EFI_ERROR (Status);
   }
 

UefiPayloadPkg/Include/Library/BlParseLib.h
@@ -2,7 +2,7 @@
   This library will parse the coreboot table in memory and extract those required
   information.
 
-  Copyright (c) 2014 - 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2014 - 2021, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -10,8 +10,8 @@
 #include <Guid/GraphicsInfoHob.h>
 #include <Guid/MemoryMapInfoGuid.h>
 #include <Guid/SerialPortInfoGuid.h>
-#include <Guid/AcpiTableGuid.h>
-#include <Guid/SmbiosTableGuid.h>
+#include <Guid/SmBiosTableHob.h>
+#include <Guid/AcpiTableHob.h>
 #include <Guid/SmramMemoryReserve.h>
 #include <Guid/SmmRegisterInfoGuid.h>
 #include <Guid/SmmS3CommunicationInfoGuid.h>
@@ -73,7 +73,7 @@ ParseMemoryInfo (
 RETURN_STATUS
 EFIAPI
 ParseSmbiosTable (
-  OUT SMBIOS_TABLE_HOB     *SmbiosTable
+  OUT PLD_SMBIOS_TABLE_HOB  *SmbiosTable
   );
 
 /**
@@ -88,7 +88,7 @@ ParseSmbiosTable (
 RETURN_STATUS
 EFIAPI
 ParseAcpiTableInfo (
-  OUT ACPI_TABLE_HOB        *AcpiTableHob
+  OUT PLD_ACPI_TABLE_HOB   *AcpiTableHob
   );
 
 

UefiPayloadPkg/Library/AcpiTimerLib/AcpiTimerLib.c
@@ -13,7 +13,7 @@
 #include <Library/HobLib.h>
 #include <Library/DebugLib.h>
 #include <IndustryStandard/Acpi.h>
-#include <Guid/AcpiTableGuid.h>
+#include <Guid/AcpiTableHob.h>
 
 #define ACPI_TIMER_COUNT_SIZE  BIT24
 
@@ -84,16 +84,16 @@ AcpiTimerLibConstructor (
   )
 {
   EFI_HOB_GUID_TYPE  *GuidHob;
-  ACPI_TABLE_HOB     *AcpiTableHob;
+  PLD_ACPI_TABLE_HOB *AcpiTableHob;
 
   //
   // Find the acpi table information guid hob
   //
-  GuidHob = GetFirstGuidHob (&gEfiAcpiTableGuid);
+  GuidHob = GetFirstGuidHob (&gPldAcpiTableGuid);
   ASSERT (GuidHob != NULL);
 
-  AcpiTableHob = (ACPI_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
-  mPmTimerReg = (UINTN)GetPmTimerRegister (AcpiTableHob->TableAddress);
+  AcpiTableHob = (PLD_ACPI_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
+  mPmTimerReg = (UINTN)GetPmTimerRegister ((UINT64)(UINTN)AcpiTableHob->Rsdp);
 
   return EFI_SUCCESS;
 }

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -1,6 +1,6 @@
 /** @file
 
-  Copyright (c) 2014 - 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2014 - 2021, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -64,14 +64,14 @@ BuildHobFromBl (
   )
 {
   EFI_STATUS                       Status;
-  ACPI_TABLE_HOB                   AcpiTableHob;
-  ACPI_TABLE_HOB                   *NewAcpiTableHob;
-  SMBIOS_TABLE_HOB                 SmbiosTable;
-  SMBIOS_TABLE_HOB                 *NewSmbiosTable;
   EFI_PEI_GRAPHICS_INFO_HOB        GfxInfo;
   EFI_PEI_GRAPHICS_INFO_HOB        *NewGfxInfo;
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB GfxDeviceInfo;
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB *NewGfxDeviceInfo;
+  PLD_SMBIOS_TABLE_HOB             SmbiosTable;
+  PLD_SMBIOS_TABLE_HOB             *NewSmbiosTable;
+  PLD_ACPI_TABLE_HOB               AcpiTableHob;
+  PLD_ACPI_TABLE_HOB               *NewAcpiTableHob;
 
   //
   // Parse memory info and build memory HOBs
@@ -107,10 +107,10 @@ BuildHobFromBl (
   //
   Status = ParseSmbiosTable(&SmbiosTable);
   if (!EFI_ERROR (Status)) {
-    NewSmbiosTable = BuildGuidHob (&gEfiSmbiosTableGuid, sizeof (SmbiosTable));
+    NewSmbiosTable = BuildGuidHob (&gPldSmbiosTableGuid, sizeof (SmbiosTable));
     ASSERT (NewSmbiosTable != NULL);
     CopyMem (NewSmbiosTable, &SmbiosTable, sizeof (SmbiosTable));
-    DEBUG ((DEBUG_INFO, "Detected Smbios Table at 0x%lx\n", SmbiosTable.TableAddress));
+    DEBUG ((DEBUG_INFO, "Detected Smbios Table at 0x%lx\n", SmbiosTable.SmBiosEntryPoint));
   }
 
   //
@@ -119,10 +119,10 @@ BuildHobFromBl (
   Status = ParseAcpiTableInfo(&AcpiTableHob);
   ASSERT_EFI_ERROR (Status);
   if (!EFI_ERROR (Status)) {
-    NewAcpiTableHob = BuildGuidHob (&gEfiAcpiTableGuid, sizeof (AcpiTableHob));
+    NewAcpiTableHob = BuildGuidHob (&gPldAcpiTableGuid, sizeof (AcpiTableHob));
     ASSERT (NewAcpiTableHob != NULL);
     CopyMem (NewAcpiTableHob, &AcpiTableHob, sizeof (AcpiTableHob));
-    DEBUG ((DEBUG_INFO, "Detected ACPI Table at 0x%lx\n", AcpiTableHob.TableAddress));
+    DEBUG ((DEBUG_INFO, "Detected ACPI Table at 0x%lx\n", AcpiTableHob.Rsdp));
   }
 
 

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.h
@@ -27,8 +27,8 @@
 #include <Guid/SerialPortInfoGuid.h>
 #include <Guid/MemoryMapInfoGuid.h>
 #include <Guid/GraphicsInfoHob.h>
-#include <Guid/AcpiTableGuid.h>
-#include <Guid/SmbiosTableGuid.h>
+#include <Guid/SmBiosTableHob.h>
+#include <Guid/AcpiTableHob.h>
 
 
 #define LEGACY_8259_MASK_REGISTER_MASTER  0x21

thiyagukb/edk2-1

UefiPayloadPkg/BlSupportDxe/BlSupportDxe.c
@@ -8,7 +8,7 @@
 #include "BlSupportDxe.h"
 
 
-
+extern EFI_GUID gUniversalPayloadAcpiTableGuid;
 
 /**
   Set the platform related PCDs using ACPI table
@@ -176,34 +176,19 @@ BlDxeEntryPoint (
   EFI_STATUS Status;
   EFI_HOB_GUID_TYPE          *GuidHob;
   EFI_PEI_GRAPHICS_INFO_HOB  *GfxInfo;
-  ACPI_TABLE_HOB             *AcpiTableHob;
-  SMBIOS_TABLE_HOB           *SmbiosTable;
+  ACPI_TABLE_HOB         *AcpiTableHob;
 
   Status = EFI_SUCCESS;
 
   //
   // Install Acpi Table
   //
-  GuidHob = GetFirstGuidHob (&gEfiAcpiTableGuid);
+  GuidHob = GetFirstGuidHob (&gUniversalPayloadAcpiTableGuid);
   ASSERT (GuidHob != NULL);
   if (GuidHob != NULL) {
     AcpiTableHob = (ACPI_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
     DEBUG ((DEBUG_ERROR, "Install Acpi Table at 0x%lx \n", AcpiTableHob->TableAddress));
-    Status = gBS->InstallConfigurationTable (&gEfiAcpiTableGuid, (VOID *)(UINTN)AcpiTableHob->TableAddress);
-    ASSERT_EFI_ERROR (Status);
-
-    Status = SetPcdsUsingAcpiTable (AcpiTableHob->TableAddress);
-    ASSERT_EFI_ERROR (Status);
-  }
-
-  //
-  // Install Smbios Table
-  //
-  GuidHob = GetFirstGuidHob (&gEfiSmbiosTableGuid);
-  if (GuidHob != NULL) {
-    SmbiosTable = (SMBIOS_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
-    DEBUG ((DEBUG_ERROR, "Install SMBIOS Table at 0x%lx \n", SmbiosTable->TableAddress));
-    Status = gBS->InstallConfigurationTable (&gEfiSmbiosTableGuid, (VOID *)(UINTN)SmbiosTable->TableAddress);
+    Status = SetPcdsUsingAcpiTable ((UINT64)(UINTN)AcpiTableHob->TableAddress);
     ASSERT_EFI_ERROR (Status);
   }
 

UefiPayloadPkg/Include/Library/BlParseLib.h
@@ -2,7 +2,7 @@
   This library will parse the coreboot table in memory and extract those required
   information.
 
-  Copyright (c) 2014 - 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2014 - 2021, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -10,8 +10,8 @@
 #include <Guid/GraphicsInfoHob.h>
 #include <Guid/MemoryMapInfoGuid.h>
 #include <Guid/SerialPortInfoGuid.h>
+#include <Guid/SmBiosTableHob.h>
 #include <Guid/AcpiTableGuid.h>
-#include <Guid/SmbiosTableGuid.h>
 #include <Guid/SmramMemoryReserve.h>
 #include <Guid/SmmRegisterInfoGuid.h>
 #include <Guid/SmmS3CommunicationInfoGuid.h>
@@ -73,7 +73,7 @@ ParseMemoryInfo (
 RETURN_STATUS
 EFIAPI
 ParseSmbiosTable (
-  OUT SMBIOS_TABLE_HOB     *SmbiosTable
+  OUT PLD_SMBIOS_TABLE_HOB  *SmbiosTable
   );
 
 /**
@@ -88,7 +88,7 @@ ParseSmbiosTable (
 RETURN_STATUS
 EFIAPI
 ParseAcpiTableInfo (
-  OUT ACPI_TABLE_HOB        *AcpiTableHob
+  OUT ACPI_TABLE_HOB   *AcpiTableHob
   );
 
 

UefiPayloadPkg/Library/AcpiTimerLib/AcpiTimerLib.c
@@ -17,6 +17,8 @@
 
 #define ACPI_TIMER_COUNT_SIZE  BIT24
 
+
+extern EFI_GUID gUniversalPayloadAcpiTableGuid;
 UINT32   mPmTimerReg = 0;
 
 /**
@@ -84,16 +86,16 @@ AcpiTimerLibConstructor (
   )
 {
   EFI_HOB_GUID_TYPE  *GuidHob;
-  ACPI_TABLE_HOB     *AcpiTableHob;
+  ACPI_TABLE_HOB *AcpiTableHob;
 
   //
   // Find the acpi table information guid hob
   //
-  GuidHob = GetFirstGuidHob (&gEfiAcpiTableGuid);
+  GuidHob = GetFirstGuidHob (&gUniversalPayloadAcpiTableGuid);
   ASSERT (GuidHob != NULL);
 
   AcpiTableHob = (ACPI_TABLE_HOB *)GET_GUID_HOB_DATA (GuidHob);
-  mPmTimerReg = (UINTN)GetPmTimerRegister (AcpiTableHob->TableAddress);
+  mPmTimerReg = (UINTN)GetPmTimerRegister ((UINT64)(UINTN)AcpiTableHob->TableAddress);
 
   return EFI_SUCCESS;
 }

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.c
@@ -64,14 +64,14 @@ BuildHobFromBl (
   )
 {
   EFI_STATUS                       Status;
-  ACPI_TABLE_HOB                   AcpiTableHob;
-  ACPI_TABLE_HOB                   *NewAcpiTableHob;
-  SMBIOS_TABLE_HOB                 SmbiosTable;
-  SMBIOS_TABLE_HOB                 *NewSmbiosTable;
   EFI_PEI_GRAPHICS_INFO_HOB        GfxInfo;
   EFI_PEI_GRAPHICS_INFO_HOB        *NewGfxInfo;
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB GfxDeviceInfo;
   EFI_PEI_GRAPHICS_DEVICE_INFO_HOB *NewGfxDeviceInfo;
+  PLD_SMBIOS_TABLE_HOB             SmbiosTable;
+  PLD_SMBIOS_TABLE_HOB             *NewSmbiosTable;
+  PLD_ACPI_TABLE_HOB               AcpiTableHob;
+  PLD_ACPI_TABLE_HOB               *NewAcpiTableHob;
 
   //
   // Parse memory info and build memory HOBs
@@ -107,10 +107,10 @@ BuildHobFromBl (
   //
   Status = ParseSmbiosTable(&SmbiosTable);
   if (!EFI_ERROR (Status)) {
-    NewSmbiosTable = BuildGuidHob (&gEfiSmbiosTableGuid, sizeof (SmbiosTable));
+    NewSmbiosTable = BuildGuidHob (&gUniversalPayloadSmbiosTableGuid, sizeof (SmbiosTable));
     ASSERT (NewSmbiosTable != NULL);
     CopyMem (NewSmbiosTable, &SmbiosTable, sizeof (SmbiosTable));
-    DEBUG ((DEBUG_INFO, "Detected Smbios Table at 0x%lx\n", SmbiosTable.TableAddress));
+    DEBUG ((DEBUG_INFO, "Detected Smbios Table at 0x%lx\n", SmbiosTable.SmBiosEntryPoint));
   }
 
   //
@@ -119,10 +119,10 @@ BuildHobFromBl (
   Status = ParseAcpiTableInfo(&AcpiTableHob);
   ASSERT_EFI_ERROR (Status);
   if (!EFI_ERROR (Status)) {
-    NewAcpiTableHob = BuildGuidHob (&gEfiAcpiTableGuid, sizeof (AcpiTableHob));
+    NewAcpiTableHob = BuildGuidHob (&gUniversalPayloadAcpiTableGuid, sizeof (AcpiTableHob));
     ASSERT (NewAcpiTableHob != NULL);
     CopyMem (NewAcpiTableHob, &AcpiTableHob, sizeof (AcpiTableHob));
-    DEBUG ((DEBUG_INFO, "Detected ACPI Table at 0x%lx\n", AcpiTableHob.TableAddress));
+    DEBUG ((DEBUG_INFO, "Detected ACPI Table at 0x%lx\n", AcpiTableHob.Rsdp));
   }
 
 

UefiPayloadPkg/UefiPayloadEntry/UefiPayloadEntry.h
@@ -27,13 +27,11 @@
 #include <Guid/SerialPortInfoGuid.h>
 #include <Guid/MemoryMapInfoGuid.h>
 #include <Guid/GraphicsInfoHob.h>
-#include <UniversalPayload/SmbiosTable.h>
-#include <UniversalPayload/AcpiTable.h>
-#include <UniversalPayload/UniversalPayload.h>
-#include <UniversalPayload/ExtraData.h>
-#include <Guid/PcdDataBaseSignatureGuid.h>
+#include <Guid/SmBiosTableHob.h>
 #include <Guid/AcpiTableGuid.h>
-#include <Guid/SmbiosTableGuid.h>
+#include <UniversalPayload/UniversalPayload.h>
+#include <Guid/PcdDataBaseSignatureGuid.h>
+#include <UniversalPayload/ExtraData.h>
 
 
 #define LEGACY_8259_MASK_REGISTER_MASTER  0x21